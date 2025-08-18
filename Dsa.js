bafdf62e47bf14776b446746dacd4aa7e234b152
prasann <prasanntripathi83050@gmail.com>
2025-08-18 23:28:45 +0530

    First day commit

---
+++ Dsa.js
@@ -0,0 +1,74 @@
+//  FUNCTIONS AND IF-ELSE STATEMENTS
+
+
+// function greet () {
+//     console.log("Hello, DSA!");
+// }
+
+// greet();  // Calling/invoking the function to see the output
+
+// function greet(name){
+//      console.log("Hello," + name + "!");
+// }
+
+// greet("DSA");  // Calling the function with an argument
+// greet("Alice");  // Calling the function with another argument
+
+
+// function greet (name) {
+//      console.log("Hello, " + name + "!");
+// }
+
+// let name = "Prasann Tripathi"
+// greet(name);  // Calling the function with a variable
+
+// function sum(a, b){
+//      return a + b;  // Returns the sum of a and b
+// }
+// let result = sum(5, 10);  // Calling the function and storing the result
+// console.log(result);  // Output: 15
+
+
+// function sum (a,b){
+//      let add = a + b;  
+//      console.log("The sum is: " + add);  // Output the sum
+// }
+// sum(5, 10);  // Calling the function with two numbers
+
+
+// function multiply(a,b){
+//      let product = a * b;
+//      return product;  // Returns the product of a and b
+// }
+// let result = multiply(5, 10);  // Calling the function and storing the result
+// console.log("The product is: " + result);  // Output: The product is:
+
+
+// function EligibleForVoting(age){
+//      if(age >= 18){
+//           console.log("You are eligible for voting.");
+//      }else{
+//           console.log("You are not eligible for voting.");
+//           }
+// }
+// EligibleForVoting(10);  // Calling the function with an age that meets the condition
+
+
+// function EligibleForVoting(age){
+//      if(age < 0){
+//           console.log("Age cannot be negative.");
+//      }else if(age >= 18){
+//           console.log("You are eligible for voting.");}else{
+//           console.log("You are not eligible for voting.");}
+// }
+// EligibleForVoting(-2);  // Calling the function with an age that does not meet the condition
+
+// function checkEvenOdd(number){
+//      if(number % 2 === 0){
+//           console.log(number + " is an even number.");
+//      }else{
+//           console.log(number + " is an odd number.");
+//           }
+// }
+
+// checkEvenOdd(10);  // Calling the function with an even number
