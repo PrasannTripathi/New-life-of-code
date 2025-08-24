4989f1972357fe002d37cb1841b3f2c5cbe6e119
prasann <prasanntripathi83050@gmail.com>
2025-08-24 10:42:58 +0530

    6th day commit

--- Dsa.js
+++ Dsa.js
@@ -354,3 +354,122 @@
 //         console.log(`i = ${i}, j = ${j}`)
 //     }
 // }
+
+// ***************  6TH DAY PRACTICE  ******************
+
+// for (let i = 0; i < 5; i++){
+//     for (let j = 0; j <= i; j++){
+//         console.log(`i = ${i}, j = ${j}`)
+//     }
+// }
+
+// for (let i = 0; i < 5; i++){
+//     for (let j = i; j > 0; j--){
+//         console.log(`i = ${i}, j = ${j}`)
+//     }
+// }
+
+// for (let i = 0; i < 3; i++){
+//     for (let j = i; j > 0; j--){
+//         console.log(`i = ${i}, j = ${j}`)
+//     }
+// }
+
+// for (let i = 5; i > 0; i--){
+//     for (let j = 0; j < i; j++){
+//         console.log(`i = ${i}, j = ${j}`)
+//     }
+// }
+
+
+// **********     STAR PATTERN    *************************************
+
+// let n = 5
+// for (let i = 0; i < n; i++){
+//     let row = ""
+//     for (let j = 0; j < n; j++){
+//         row = row + " * "
+//     }
+//     console.log(row)
+// }
+
+
+// let n = 4;
+// for (let i = 0; i<n; i++) {
+//     let row = "";
+//     for (let j = 0; j < i + 1; j++){
+//         row = row + " * "
+//     }
+//     console.log(row)
+// }
+
+// let n = 4;
+// for (let i = 0; i<n; i++) {
+//     let row = "";
+//     for (let j = 0; j <= i ; j++){
+//         row = row + " * "
+//     }
+//     console.log(row)
+// }
+
+
+// let n = 4;
+// for (let i = 0; i<n; i++) {
+//     let row = "";
+//     for (let j = 0; j <= i ; j++){
+//         row = row + (j + 1)
+//     }
+//     console.log(row)
+// }
+
+
+// let n = 4;
+// for (let i = 0; i < n; i++) {
+//     let row = "";
+//     for (let j = 0; j <= i; j++) {
+//         row = row + (i + 1)
+//     }
+//     console.log(row)
+// }
+
+
+// let n = 4;
+// for (let i = 1; i <= n; i++) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//         row = row + (i)
+//     }
+//     console.log(row)
+// }
+
+
+// let n = 4;
+// for (let i = 0; i < n; i++) {
+//     let row = "";
+//     for (let j = 0; j < n-i; j++) {
+//         row = row + (j + 1)
+//     }
+//     console.log(row)
+// }
+
+// let n = 4;
+// for (let i = 0; i < n; i++) {
+//     let row = "";
+//     for (let j = 0; j < n-i; j++) {
+//         row = row + " * "
+//     }
+//     console.log(row)
+// }
+
+
+// let n = 5;
+// for (let i = 0; i < n; i++){
+//     let row = ""
+//     for (let j = 0; j < n - (i + 1); j++){
+//         row = row + " "
+//     }
+//     for (let k = 0; k < (i + 1); k++){
+//         row = row + '*'
+//     }
+//     console.log(row)
+// }
